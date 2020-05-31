590b0c3e8ad6f0d1f2796a52fef16693e0787815
axbarsan <mnlk123@yahoo.com>
2020-05-31 03:05:51 +0200

    Add basic token types

---
+++ token/token.go
@@ -0,0 +1,73 @@
+package token
+
+// These are the tokens that our lexer will extract from the source code.
+
+type Type string
+
+type Token struct {
+	// Type represents the type of the token (e.g. FUNCTION).
+	Type Type
+	// Literal represents the token, but in a literal fashion (e.g. func).
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL" // An unknown token.
+	EOF     = "EOF"     // End of file.
+
+	// Identifiers and literals.
+	IDENT = "IDENT" // Function / variable name.
+	INT   = "INT"   // Integer.
+
+	// Operators.
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters.
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords.
+	FUNCTION = "FUNCTION"
+	CONST    = "CONST"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var (
+	keywords = map[string]Type{
+		"fn":     FUNCTION,
+		"const":  CONST,
+		"true":   TRUE,
+		"false":  FALSE,
+		"if":     IF,
+		"else":   ELSE,
+		"return": RETURN,
+	}
+)
+
+func LookupIdentifier(identifier string) Type {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+
+	return IDENT
+}
