2cd0dff15dc7b5cdb3a695e1c2a98bb7c4e4dca9
axbarsan <mnlk123@yahoo.com>
2020-05-31 03:07:08 +0200

    Create main that runs the repl

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/axbarsan/doggo/repl"
+)
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the doggo programming language!\n", u.Name)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
