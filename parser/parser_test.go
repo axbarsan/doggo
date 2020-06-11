2b65321affc2041a4180135efc47b6f80fe8a396
axbarsan <mnlk123@yahoo.com>
2020-06-11 23:07:52 +0200

    Add test for parsing function call expressions

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -654,7 +654,71 @@ func TestFunctionParameterParsing(t *testing.T) {
 		function := stmt.Expression.(*ast.FunctionLiteral)
 
 		if len(function.Parameters) != len(tc.expectedParams) {
-			t.Errorf("length parameters wrong. want %d, got=%d\n", len(tc.expectedParams), len(function.Parameters))
+			t.Errorf("wrong parameters length. want %d, got=%d\n", len(tc.expectedParams), len(function.Parameters))
+		}
+
+		for i, ident := range tc.expectedParams {
+			testLiteralExpression(t)(function.Parameters[i], ident)
+		}
+	}
+}
+
+func TestCallExpressionParsing(t *testing.T) {
+	input := "add(1, 2 * 3, 4 + 5);"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t)(p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n", 1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("stmt is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.CallExpression. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t)(exp.Function, "add") {
+		return
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("wrong length of arguments. got=%d", len(exp.Arguments))
+	}
+
+	testLiteralExpression(t)(exp.Arguments[0], 1)
+	testInfixExpression(t)(exp.Arguments[1], 2, "*", 3)
+	testInfixExpression(t)(exp.Arguments[2], 4, "+", 5)
+}
+
+func TestCallExpressionParametersParsing(t *testing.T) {
+	testCases := []struct {
+		input          string
+		expectedParams []string
+	}{
+		{input: "add();", expectedParams: nil},
+		{input: "add(1);", expectedParams: []string{"1"}},
+		{input: "add(2 * 3, 4 * 5);", expectedParams: []string{"2 * 3", "4 * 5"}},
+	}
+
+	for _, tc := range testCases {
+		l := lexer.New(tc.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t)(p)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+		function := stmt.Expression.(*ast.FunctionLiteral)
+
+		if len(function.Parameters) != len(tc.expectedParams) {
+			t.Errorf("wrong parameters length. want %d, got=%d\n", len(tc.expectedParams), len(function.Parameters))
 		}
 
 		for i, ident := range tc.expectedParams {
