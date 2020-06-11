a6ba8985111045a8874c0096eb2ef728bc090663
axbarsan <mnlk123@yahoo.com>
2020-06-11 23:07:27 +0200

    Add ast for function calls

---
+++ ast/fn_call.go
@@ -0,0 +1,35 @@
+package ast
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/axbarsan/doggo/token"
+)
+
+type CallExpression struct {
+	Token     token.Token // The '(' token.
+	Function  Expression  // Identifier or FunctionLiteral.
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+
+func (ce *CallExpression) String() string {
+	var args []string
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	var out bytes.Buffer
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
