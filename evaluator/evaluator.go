e8e17e1674cad1b91084589ef790d16c2129059b
axbarsan <mnlk123@yahoo.com>
2020-06-21 02:00:11 +0200

    Add evaluation for infix expressions that return numbers (such as addition or multiplication)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -30,6 +30,12 @@ func Eval(node ast.Node) object.Object {
 
 		return evalPrefixExpression(node.Operator, right)
 
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+
+		return evalInfixExpression(node.Operator, left, right)
+
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	}
@@ -94,4 +100,44 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	}
 
 	return obj
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{
+			Value: leftVal + rightVal,
+		}
+
+	case "-":
+		return &object.Integer{
+			Value: leftVal - rightVal,
+		}
+
+	case "*":
+		return &object.Integer{
+			Value: leftVal * rightVal,
+		}
+
+	case "/":
+		return &object.Integer{
+			Value: leftVal / rightVal,
+		}
+
+	default:
+		return NULL
+	}
 }
