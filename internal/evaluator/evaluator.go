cc1179c8bc7862bde765cab8457b1f98e89d8ac7
axbarsan <mnlk123@yahoo.com>
2020-06-27 00:28:04 +0200

    Add support for string evaluation

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -25,6 +25,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
 	case *ast.PrefixExpression:
 		right := Eval(node.Right, env)
 		if isError(right) {
