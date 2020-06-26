75729ce6e3ad8d498c139d3715a6d984e4f49dac
axbarsan <mnlk123@yahoo.com>
2020-06-26 23:10:29 +0200

    Add support for parsing strings

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -46,6 +46,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.Type]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.STRING, p.parseStringLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
@@ -237,6 +238,15 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	}
 
 	literal.Value = value
+
+	return literal
+}
+
+func (p *Parser) parseStringLiteral() ast.Expression {
+	literal := &ast.StringLiteral{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
 
 	return literal
 }
