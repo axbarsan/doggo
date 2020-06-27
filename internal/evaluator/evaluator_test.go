ac4565d280eb6dcfda45a704d3be239cc18dcdda
axbarsan <mnlk123@yahoo.com>
2020-06-27 23:35:00 +0200

    Add support for evaluating arrays and array index expressions

--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -403,3 +403,79 @@ func TestBuiltinFunctions(t *testing.T) {
 		}
 	}
 }
+
+func TestArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	evaluated := testEval(input)
+	result, ok := evaluated.(*object.Array)
+	if !ok {
+		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(result.Elements) != 3 {
+		t.Fatalf("array has wrong num of elements. got=%d", len(result.Elements))
+	}
+
+	testIntegerObject(t)(result.Elements[0], 1)
+	testIntegerObject(t)(result.Elements[1], 4)
+	testIntegerObject(t)(result.Elements[2], 6)
+}
+
+func TestArrayIndexExpressions(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			"[1, 2, 3][0]",
+			1,
+		},
+		{
+			"[1, 2, 3][1]",
+			2,
+		},
+		{
+			"[1, 2, 3][2]",
+			3,
+		},
+		{
+			"const i = 0; [1][i];",
+			1,
+		},
+		{
+			"[1, 2, 3][1 + 1];",
+			3,
+		},
+		{
+			"const myArray = [1, 2, 3]; myArray[2];",
+			3,
+		},
+		{
+			"const myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];",
+			6,
+		},
+		{
+			"const myArray = [1, 2, 3]; const i = myArray[0]; myArray[i]",
+			2,
+		},
+		{
+			"[1, 2, 3][3]",
+			nil,
+		},
+		{
+			"[1, 2, 3][-1]",
+			nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		evaluated := testEval(tc.input)
+		integer, ok := tc.expected.(int)
+		if ok {
+			testIntegerObject(t)(evaluated, int64(integer))
+		} else {
+			testNullObject(t)(evaluated)
+		}
+	}
+}
---
+++ internal/object/array.go
@@ -0,0 +1,33 @@
+package object
+
+import (
+	"bytes"
+	"strings"
+)
+
+const (
+	ARRAY_OBJ = "ARRAY"
+)
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() Type {
+	return ARRAY_OBJ
+}
+
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	var elements []string
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
